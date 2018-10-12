9f1ec7a16b5c79fe26fe04ff35e971fe03a1fa35
chenweida <494679975@qq.com>
2018-10-12 15:32:09 +0800

    首次代码提交

--- simple_demo3/src/router/index.js
+++ simple_demo3/src/router/index.js
@@ -1,9 +1,12 @@
 import Vue from 'vue'
 import Router from 'vue-router'
+import ElementUI from 'element-ui'
+import 'element-ui/lib/theme-chalk/index.css'
 import HelloWorld from '@/components/HelloWorld'
 import personInfo from '@/components/person/personInfo'
 import personList from '@/components/person/personList'
 
+Vue.use(ElementUI)
 Vue.use(Router)
 
 export default new Router({
